7eb250f52a865b4f51e7b6d545d92579f6da8322
eric <ericstrong89@gmail.com>
2017-10-06 13:40:06 +0100

    lab2-python solution 10

--- CloudLab2/Python/Swapi10.py
+++ CloudLab2/Python/Swapi10.py
@@ -2,19 +2,32 @@
 """What   what   is   the   producer’s   name
 who   has   been   involved   the   greatest number   of   films?"""
 import requests
+import sqlite3
+
+
+#create database
+db = sqlite3.connect('database2.db')
+c = db.cursor()
+c.execute('drop table if exists producers')
+c.execute('create table producers(producername TEXT)')
 
 listofproducers = []
 
 def producersnames(record):
     producers = record.get('producer')
+
     title = record.get('title')
-    # print(producers)
-    # print(title)
+    p = producers.split(",")
+
+    for i in p:
+        # print(i)
+        sqlInsert10 = "insert into producers values('{}')".format(i)
+        c.execute(sqlInsert10)
+        db.commit()
+
 
     t = (producers, title)
     listofproducers.append(t)
-
-
 
 numberofRecords = 0
 pageNumber = 1 # for part 1
@@ -43,3 +56,12 @@ while(True):
     pageNumber +=1
 ################################
 print(listofproducers)
+
+
+# returning the data set for number of producers that meet the requirment
+c.execute("SELECT  producername, count(producername) from producers group by producername  ORDER BY count(producername) DESC" )
+result = c.fetchall()
+topProducer = result[0]
+
+print('producer associated with the most movies is:', topProducer[0])
+
